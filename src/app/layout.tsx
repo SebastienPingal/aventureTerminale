88386f48fe525ac093694165546cb59c9ab87069
Sebastien <sebastien.pingal@gmail.com>
2025-07-15 12:56:15 +0200

    🎨 Update layout to use Rubik Dirt font and change header title to "Un Désert"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,39 @@
-import type { Metadata } from "next";
-import { Syne_Mono, Zen_Dots } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import { AuthSessionProvider } from "@/components/SessionProvider";
-import { Separator } from "@/components/ui/separator";
-import { ToggleTheme } from "@/components/ToggleTheme";
-import { auth } from "@/auth";
+import type { Metadata } from "next"
+import { Syne_Mono, Rubik_Dirt } from "next/font/google"
+import "./globals.css"
+import { ThemeProvider } from "@/components/ThemeProvider"
+import { AuthSessionProvider } from "@/components/SessionProvider"
+import { Separator } from "@/components/ui/separator"
+import { ToggleTheme } from "@/components/ToggleTheme"
+import { auth } from "@/auth"
 
-const zenDots = Zen_Dots({
-  variable: "--font-zen-dots",
+const rubikDirt = Rubik_Dirt({
+  variable: "--font-rubik-dirt",
   subsets: ["latin"],
   weight: ["400"],
-});
+})
 
 const syneMono = Syne_Mono({
   variable: "--font-syne-mono",
   subsets: ["latin"],
   weight: ["400"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Aventure Terminale",
   description: "Aventure Terminale",
-};
+}
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-
   const session = await auth()
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${zenDots.variable} ${syneMono.variable} antialiased h-screen overflow-hidden relative flex flex-col`}
+        className={`${rubikDirt.variable} ${syneMono.variable} antialiased h-screen overflow-hidden relative flex flex-col font-[family-name:var(--font-syne-mono)]`}
       >
         <AuthSessionProvider >
           <ThemeProvider
@@ -45,9 +44,10 @@ export default async function RootLayout({
           >
             <header className="bg-background w-full pt-5 px-4">
               <div className="flex justify-between items-center max-w-6xl mx-auto">
-                <h1 className="text-4xl font-bold font-[family-name:var(--font-zen-dots)]">
-                  Aventure Terminale
+                <h1 className="text-4xl font-[family-name:var(--font-rubik-dirt)]">
+                  Un Désert
                 </h1>
+
                 <div className="flex items-center gap-2">
                   {session && <p>{session.user?.name}</p>}
                   <ToggleTheme />
@@ -56,12 +56,12 @@ export default async function RootLayout({
               <Separator className="w-full" />
             </header>
 
-            <div className="flex flex-col items-center max-w-6xl mx-auto flex-1 overflow-y-auto">
+            <div className="flex flex-col items-center max-w-6xl mx-auto flex-1 overflow-y-auto w-full">
               {children}
             </div>
           </ThemeProvider>
         </AuthSessionProvider>
       </body>
     </html>
-  );
+  )
 }
