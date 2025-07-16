b03a4d59b644810ff5a87b81c85eaa1e8ecbb45c
Sebastien <sebastien.pingal@gmail.com>
2025-07-16 13:15:23 +0200

    ✨ Add getMe function to retrieve current user session data

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -3,12 +3,13 @@
 import { Prisma } from "@/app/generated/prisma"
 import prisma from "@/lib/prisma"
 import { User } from "@/lib/types"
+import { auth } from "@/auth"
 
 export async function getUser(id: Prisma.UserWhereUniqueInput): Promise<User | null> {
   const user = await prisma.user.findUnique({
     where: id,
   })
- 
+
   return user
 }
 
@@ -19,4 +20,15 @@ export async function updateUser(id: Prisma.UserWhereUniqueInput, data: Prisma.U
   })
 
   return user
+}
+
+export async function getMe(): Promise<User | null> {
+  const session = await auth()
+  if (!session?.user) {
+    return null
+  }
+
+  const me = await getUser({ id: session.user.id })
+
+  return me
 }
