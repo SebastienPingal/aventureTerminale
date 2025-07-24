0096ef1186f5b79c903725bddab007190b4cb4bd
Sebastien <sebastien.pingal@gmail.com>
2025-07-24 11:35:33 +0200

    ♻️  Refactor layout to include Header component; streamline user session display and theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@ import { Syne_Mono, Rubik_Dirt } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/ThemeProvider"
 import { AuthSessionProvider } from "@/components/SessionProvider"
-import { Separator } from "@/components/ui/separator"
-import { ToggleTheme } from "@/components/ToggleTheme"
-import { auth, signOut } from "@/auth"
+import Header from "@/components/Header"
 
 const rubikDirt = Rubik_Dirt({
   variable: "--font-rubik-dirt",
@@ -29,8 +27,7 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const session = await auth()
-  
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
@@ -43,34 +40,7 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <header className="bg-background w-full pt-5 px-4">
-              <div className="flex justify-between items-center max-w-6xl mx-auto">
-                <h1 className="text-4xl font-[family-name:var(--font-rubik-dirt)]">
-                  Un Désert
-                </h1>
-
-                <div className="flex items-center gap-2">
-                  {session && (
-                    <div className="flex items-center gap-2 text-sm">
-                      <p>{session.user?.name}</p>
-                      <form action={async () => {
-                        "use server"
-                        await signOut()
-                      }}>
-                        <button
-                          type="submit"
-                          className="text-sm underline cursor-pointer"
-                        >
-                          Logout
-                        </button>
-                      </form>
-                    </div>
-                  )}
-                  <ToggleTheme />
-                </div>
-              </div>
-              <Separator className="w-full" />
-            </header>
+            <Header />
 
             <div className="flex flex-col items-center max-w-6xl mx-auto flex-1 overflow-y-auto w-full">
               {children}
---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { Separator } from "@/components/ui/separator"
+import { ToggleTheme } from "./ToggleTheme"
+import { auth } from "@/auth"
+import { signOut } from "next-auth/react"
+import { getMe } from "@/actions/user"
+
+export default async function Header() {
+  const session = await auth()
+  const user = session ? await getMe() : null
+  const userWorldCell = user?.worldCell
+
+  console.log('✨ userWorldCell', userWorldCell)
+
+  return (
+    <header className="bg-background w-full pt-5 px-4">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        <h1 className="text-4xl font-[family-name:var(--font-rubik-dirt)]">
+          {userWorldCell?.title || "Un Désert"}
+        </h1>
+
+        <div className="flex items-center gap-2">
+          {session && (
+            <div className="flex items-center gap-2 text-sm">
+              <p>{session.user?.name}</p>
+              <form action={async () => {
+                "use server"
+                await signOut()
+              }}>
+                <button
+                  type="submit"
+                  className="text-sm underline cursor-pointer"
+                >
+                  Logout
+                </button>
+              </form>
+            </div>
+          )}
+          <ToggleTheme />
+        </div>
+      </div>
+      <Separator className="w-full" />
+    </header>
+  )
+}
